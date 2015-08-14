d2443fe1fcdc985402308a386297cc65a1a42f0f
CharlesAMoss <charlesamoss@yahoo.com>
2015-08-14 12:09:59 -0700

    add 2nd test and fail

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -4,7 +4,8 @@
     
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
-        // 1. Input two strings and return an integer.  input: "foo", "bar"  output: 0
+        // 1. Input two strings and return an integer.  
+        // input: "foo", "bar"  output: 0
         function test_RepeatCounter_returnOutput()
         {
             
@@ -20,7 +21,30 @@
             $this->assertTrue(is_int($result));
             $this->assertEquals(0, $result);
         
-        }//end test
+        }//end test 1
+
+        // 2. Input two strings, input one and two match, the returned integer is incremented to one. 
+        // input: "foo", "foo" output: 1
+        function test_RepeatCounter_findMatch()
+        {
+            
+            //Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $input1 = "foo";
+            $input2 = "foo";
+            
+            //Act
+            $result = $test_RepeatCounter->countRepeats($input1, $input2);
+            
+            //Assert
+            $this->assertEquals(1, $result);
+        
+        }//end test 2
+
+
+
+
+
 
     }//end RepeatCounterTest 
 
